757cf4fdf97612c4a1aec1de7e72fda15efe20b5
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2023-12-22 14:24:31 +0100

    commit setting up drivers LoopBe1

--- midi_parser.py
+++ midi_parser.py
@@ -1,8 +1,16 @@
 import mido
 import time
 
-# List all available output ports
-print("Available MIDI Output Ports:")
-print(mido.get_output_names())
 
+# Open virtual MIDI ports
+outport = mido.open_output("LoopBe Internal MIDI 1")
+inport = mido.open_input("LoopBe Internal MIDI 0")
 
+# Send a MIDI message to Pure Data
+outport.send(mido.Message('note_on', note=60))
+time.sleep(1)
+outport.send(mido.Message('note_off', note=60))
+
+# Receive a MIDI message from Pure Data
+for msg in inport.iter_pending():
+    print(msg)
